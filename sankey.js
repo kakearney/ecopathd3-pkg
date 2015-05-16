63d9a9d0c9af3c23ea748d46bf37620fac72ddf8
Kelly Kearney <kakearney@gmail.com>
2015-05-15 17:39:56 -0700

    Polished ecopath sankey diagram code
    Javascript d3 code is now written as a function that can be easily run on different
    food webs, with the figure added to any DOM element.

--- sankey.js
+++ sankey.js
@@ -5,13 +5,7 @@ d3.sankey = function() {
       size = [1, 1],
       nodes = [],
       links = [],
-	  // cycle features
-	  cycleLaneNarrowWidth = 4,
-	  cycleLaneDistFromFwdPaths = -10,  // the distance above the paths to start showing 'cycle lanes'
-	  cycleDistFromNode = 30,      // linear path distance before arcing from node
-      cycleControlPointDist = 30,  // controls the significance of the cycle's arc
-	  cycleSmallWidthBuffer = 2  // distance between 'cycle lanes'
-	  ;
+      components = [];
 
   sankey.nodeWidth = function(_) {
     if (!arguments.length) return nodeWidth;
@@ -24,37 +18,6 @@ d3.sankey = function() {
     nodePadding = +_;
     return sankey;
   };
-
-  // cycle related attributes
-  sankey.cycleLaneNarrowWidth = function(_) {
-    if (!arguments.length) return cycleLaneNarrowWidth;
-    cycleLaneNarrowWidth = +_;
-    return sankey;
-  }
-
-  sankey.cycleSmallWidthBuffer = function(_) {
-    if (!arguments.length) return cycleSmallWidthBuffer;
-    cycleSmallWidthBuffer = +_;
-    return sankey;
-  }
-
-  sankey.cycleLaneDistFromFwdPaths = function(_) {
-    if (!arguments.length) return cycleLaneDistFromFwdPaths;
-    cycleLaneDistFromFwdPaths = +_;
-    return sankey;
-  }
-
-  sankey.cycleDistFromNode = function(_) {
-    if (!arguments.length) return cycleDistFromNode;
-    cycleDistFromNode = +_;
-    return sankey;
-  }
-
-  sankey.cycleControlPointDist = function(_) {
-    if (!arguments.length) return cycleControlPointDist;
-    cycleControlPointDist = +_;
-    return sankey;
-  }
 
   sankey.nodes = function(_) {
     if (!arguments.length) return nodes;
@@ -77,10 +40,13 @@ d3.sankey = function() {
   sankey.layout = function(iterations) {
     computeNodeLinks();
     computeNodeValues();
-    markCycles();
+
+    computeNodeStructure();
     computeNodeBreadths();
+
     computeNodeDepths(iterations);
     computeLinkDepths();
+    
     return sankey;
   };
 
@@ -89,59 +55,141 @@ d3.sankey = function() {
     return sankey;
   };
 
+  // A more involved path generator that requires 3 elements to render -- 
+  // It draws a starting element, intermediate and end element that are useful
+  // while drawing reverse links to get an appropriate fill.
+  //
+  // Each link is now an area and not a basic spline and no longer guarantees
+  // fixed width throughout.
+  //
+  // Sample usage:
+  //
+  //  linkNodes = this._svg.append("g").selectAll(".link")
+  //      .data(this.links)
+  //    .enter().append("g")
+  //      .attr("fill", "none")
+  //      .attr("class", ".link")
+  //      .sort(function(a, b) { return b.dy - a.dy; });
+  //
+  //  linkNodePieces = [];
+  //  for (var i = 0; i < 3; i++) {
+  //    linkNodePieces[i] = linkNodes.append("path")
+  //      .attr("class", ".linkPiece")
+  //      .attr("d", path(i))
+  //      .attr("fill", ...)
+  //  }
+  sankey.reversibleLink = function() {
+    var curvature = .5;
+
+    // Used when source is behind target, the first and last paths are simple
+    // lines at the start and end node while the second path is the spline
+    function forwardLink(part, d) {
+      var x0 = d.source.x + d.source.dx,
+          x1 = d.target.x,
+          xi = d3.interpolateNumber(x0, x1),
+          x2 = xi(curvature),
+          x3 = xi(1 - curvature),
+          y0 = d.source.y + d.sy,
+          y1 = d.target.y + d.ty,
+          y2 = d.source.y + d.sy + d.dy,
+          y3 = d.target.y + d.ty + d.dy;
+
+      switch (part) {
+        case 0:
+          return "M" + x0 + "," + y0 + "L" + x0 + "," + (y0 + d.dy);
+
+        case 1:
+          return "M" + x0 + "," + y0
+               + "C" + x2 + "," + y0 + " " + x3 + "," + y1 + " " + x1 + "," + y1
+               + "L" + x1 + "," + y3
+               + "C" + x3 + "," + y3 + " " + x2 + "," + y2 + " " + x0 + "," + y2
+               + "Z";
+      
+        case 2:
+          return "M" + x1 + "," + y1 + "L" + x1 + "," + (y1 + d.dy);
+      }
+    }
+
+    // Used for self loops and when the source is actually in front of the 
+    // target; the first element is a turning path from the source to the 
+    // destination, the second element connects the two twists and the last 
+    // twists into the target element.
+    //
+    // 
+    //  /--Target
+    //  \----------------------\
+    //                 Source--/
+    //
+    function backwardLink(part, d) {
+      var curveExtension = 30;
+      var curveDepth = 15;
+
+      function getDir(d) {
+        return d.source.y + d.sy > d.target.y + d.ty ? -1 : 1;
+      }
+
+      function p(x, y) {
+        return x + "," + y + " ";
+      }
+
+      var dt = getDir(d) * curveDepth,
+          x0 = d.source.x + d.source.dx,
+          y0 = d.source.y + d.sy,
+          x1 = d.target.x,
+          y1 = d.target.y + d.ty;
+
+      switch (part) {
+        case 0:
+          return "M" + p(x0, y0) + 
+                 "C" + p(x0, y0) +
+                       p(x0 + curveExtension, y0) +
+                       p(x0 + curveExtension, y0 + dt) +
+                 "L" + p(x0 + curveExtension, y0 + dt + d.dy) +
+                 "C" + p(x0 + curveExtension, y0 + d.dy) +
+                       p(x0, y0 + d.dy) +
+                       p(x0, y0 + d.dy) +
+                 "Z";
+        case 1:
+          return "M" + p(x0 + curveExtension, y0 + dt) + 
+                 "C" + p(x0 + curveExtension, y0 + 3 * dt) +
+                       p(x1 - curveExtension, y1 - 3 * dt) +
+                       p(x1 - curveExtension, y1 - dt) +
+                 "L" + p(x1 - curveExtension, y1 - dt + d.dy) +
+                 "C" + p(x1 - curveExtension, y1 - 3 * dt + d.dy) +
+                       p(x0 + curveExtension, y0 + 3 * dt + d.dy) +
+                       p(x0 + curveExtension, y0 + dt + d.dy) +
+                 "Z";
+
+        case 2:
+          return "M" + p(x1 - curveExtension, y1 - dt) + 
+                 "C" + p(x1 - curveExtension, y1) +
+                       p(x1, y1) +
+                       p(x1, y1) +
+                 "L" + p(x1, y1 + d.dy) +
+                 "C" + p(x1, y1 + d.dy) +
+                       p(x1 - curveExtension, y1 + d.dy) +
+                       p(x1 - curveExtension, y1 + d.dy - dt) +
+                 "Z";
+      }
+    }
+
+    return function(part) {
+      return function(d) {
+        if (d.source.x < d.target.x) {
+          return forwardLink(part, d);
+        } else {
+          return backwardLink(part, d);
+        }
+      }
+    }
+  };
+
+  // The standard link path using a constant width spline that needs a 
+  // single path element.
   sankey.link = function() {
     var curvature = .5;
 
     function link(d) {
-    if( d.causesCycle ) {
-      // cycle node; reaches backward
-
-      /*
-      The path will look like this, where
-      s=source, t=target, ?q=quadratic focus point
-     (wq)-> /-----n-----\
-            |w          |
-            |           e
-            \-t         |
-                     s--/ <-(eq)
-      */
-      // Enclosed shape using curves n' stuff
-      var smallWidth = cycleLaneNarrowWidth,
-
-	    s_x = d.source.x + d.source.dx,
-	    s_y = d.source.y + d.sy + d.dy,
-		t_x = d.target.x,
-        t_y = d.target.y,
-		se_x = s_x + cycleDistFromNode,
-		se_y = s_y,
-		ne_x = se_x,
-		ne_y = cycleLaneDistFromFwdPaths - (d.cycleIndex * (smallWidth + cycleSmallWidthBuffer) ),  // above regular paths, in it's own 'cycle lane', with a buffer around it
-		nw_x = t_x - cycleDistFromNode,
-		nw_y = ne_y,
-		sw_x = nw_x,
-		sw_y = t_y + d.ty + d.dy;
-
-      // start the path on the outer path boundary
-	  return "M" + s_x + "," + s_y
-		+ "L" + se_x + "," + se_y
-		+ "C" + (se_x + cycleControlPointDist) + "," + se_y + " " + (ne_x + cycleControlPointDist) + "," + ne_y + " " + ne_x + "," + ne_y
-		+ "H" + nw_x
-		+ "C" + (nw_x - cycleControlPointDist) + "," + nw_y + " " + (sw_x - cycleControlPointDist) + "," + sw_y + " " + sw_x + "," + sw_y
-		+ "H" + t_x
-		//moving to inner path boundary
-		+ "V" + ( t_y + d.ty )
-		+ "H" + sw_x
-		+ "C" + (sw_x - (cycleControlPointDist/2) + smallWidth) + "," + t_y + " " +
-		        (nw_x - (cycleControlPointDist/2) + smallWidth) + "," + (nw_y + smallWidth) + " " +
-				nw_x + "," + (nw_y + smallWidth)
-		+ "H" + (ne_x - smallWidth)
-		+ "C" + (ne_x + (cycleControlPointDist/2) - smallWidth) + "," + (ne_y + smallWidth) + " " +
-		        (se_x + (cycleControlPointDist/2) - smallWidth) + "," + (se_y - d.dy) + " " +
-				se_x + "," + (se_y - d.dy)
-		+ "L" + s_x + "," + (s_y - d.dy);
-
-    } else {
-      // regular forward node
       var x0 = d.source.x + d.source.dx,
           x1 = d.target.x,
           xi = d3.interpolateNumber(x0, x1),
@@ -153,7 +201,6 @@ d3.sankey = function() {
            + "C" + x2 + "," + y0
            + " " + x3 + "," + y1
            + " " + x1 + "," + y1;
-    }
     }
 
     link.curvature = function(_) {
@@ -172,6 +219,7 @@ d3.sankey = function() {
       node.sourceLinks = [];
       node.targetLinks = [];
     });
+
     links.forEach(function(link) {
       var source = link.source,
           target = link.target;
@@ -185,77 +233,184 @@ d3.sankey = function() {
   // Compute the value (size) of each node by summing the associated links.
   function computeNodeValues() {
     nodes.forEach(function(node) {
-      node.value = Math.max(
+      if (!(node.value)) //if not already given
+	  node.value = Math.max(
         d3.sum(node.sourceLinks, value),
         d3.sum(node.targetLinks, value)
       );
     });
   }
 
-  // Iteratively assign the breadth (x-position) for each node.
-  // Nodes are assigned the maximum breadth of incoming neighbors plus one;
-  // nodes with no incoming links are assigned breadth zero, while
-  // nodes with no outgoing links are assigned the maximum breadth.
-  
-  // KAK addition: allow user to assign x-position via node xPos
-  
-  // function computeNodeBreadths() {
-//     var remainingNodes = nodes,
-//         nextNodes,
-//         x = 0;
-//
-//     while (remainingNodes.length) {
-//       nextNodes = [];
-//       remainingNodes.forEach(function(node) {
-//         node.x = x;
-//         node.dx = nodeWidth;
-//         node.sourceLinks.forEach(function(link) {
-// 		  if( !link.causesCycle ) {
-//             nextNodes.push(link.target);
-// 		  }
-//         });
-//       });
-//       remainingNodes = nextNodes;
-//       ++x;
-//     }
-//
-//     moveSinksRight(x);
-//     scaleNodeBreadths((size[0] - nodeWidth) / (x - 1));
-//   }
-  
+  // Take the list of nodes and create a DAG of supervertices, each consisting 
+  // of a strongly connected component of the graph
+  //
+  // Based off:
+  // http://en.wikipedia.org/wiki/Tarjan's_strongly_connected_components_algorithm
+  function computeNodeStructure() {
+    var nodeStack = [], 
+        index = 0;
+
+    nodes.forEach(function(node) {
+      if (!node.index) {
+        connect(node);
+      }
+    });
+
+    function connect(node) {
+      node.index = index++;
+      node.lowIndex = node.index;
+      node.onStack = true;
+      nodeStack.push(node);
+
+      if (node.sourceLinks) {
+        node.sourceLinks.forEach(function(sourceLink){
+          var target = sourceLink.target;
+          if (!target.hasOwnProperty('index')) {
+            connect(target);
+            node.lowIndex = Math.min(node.lowIndex, target.lowIndex);
+          } else if (target.onStack) {
+            node.lowIndex = Math.min(node.lowIndex, target.index);
+          }
+        });
+
+        if (node.lowIndex === node.index) {
+          var component = [], currentNode;
+          do { 
+            currentNode = nodeStack.pop()
+            currentNode.onStack = false;
+            component.push(currentNode);
+          } while (currentNode != node);
+          components.push({
+            root: node,
+            scc: component
+          });
+        }
+      }
+    }
+
+    components.forEach(function(component, i){
+      component.index = i;
+      component.scc.forEach(function(node) {
+        node.component = i;
+      });
+    });
+  }
+
+  // Assign the breadth (x-position) for each strongly connected component,
+  // followed by assigning breadth within the component.
   function computeNodeBreadths() {
-      var remainingNodes = nodes,
-          nextNodes,
+    
+    layerComponents();
+
+    components.forEach(function(component, i){
+      bfs(component.root, function(node){
+        var result = node.sourceLinks
+          .filter(function(sourceLink){
+            return sourceLink.target.component == i;
+          })
+          .map(function(sourceLink){
+            return sourceLink.target;
+          });
+        return result;
+      });
+    });
+
+    var max = 0;
+    var componentsByBreadth = d3.nest()
+      .key(function(d) { return d.x; })
+      .sortKeys(d3.ascending)
+      .entries(components)
+      .map(function(d) { return d.values; });
+
+    var max = -1, nextMax = -1;
+    componentsByBreadth.forEach(function(c){
+      c.forEach(function(component){
+        component.x = max + 1;
+        component.scc.forEach(function(node){
+		  if (node.layer) node.x=node.layer;
+          else node.x = component.x + node.x;
+          nextMax = Math.max(nextMax, node.x);
+        });
+      });
+      max = nextMax;
+    });
+
+    
+    nodes
+      .filter(function(node) {
+        var outLinks = node.sourceLinks.filter(function(link){ return link.source.name != link.target.name; });
+        return (outLinks.length == 0);
+      })
+      // .forEach(function(node) { node.x = max; })  // KAK don't shift top preds to right
+
+    scaleNodeBreadths((size[0] - nodeWidth) / Math.max(max, 1));
+
+    function flatten(a) {
+      return [].concat.apply([], a);
+    }
+
+    function layerComponents() {
+      var remainingComponents = components,
+          nextComponents,
+          visitedIndex,
           x = 0;
 
-      while (remainingNodes.length) {
-        nextNodes = [];
-        remainingNodes.forEach(function(node) {
+      while (remainingComponents.length) {
+        nextComponents = [];
+        visitedIndex = {};
 
-          if (node.xPos)
-              node.x = node.xPos;
-          else
-              node.x = x;
+        remainingComponents.forEach(function(component) {
+          component.x = x;
 
-          node.dx = nodeWidth;
-          node.sourceLinks.forEach(function(link) {
-            nextNodes.push(link.target);
+          component.scc.forEach(function(n) {
+            n.sourceLinks.forEach(function(l) {
+              if (!visitedIndex.hasOwnProperty(l.target.component) &&
+                   l.target.component != component.index) {
+                nextComponents.push(components[l.target.component]);
+                visitedIndex[l.target.component] = true;
+              }
+            })
           });
         });
-        remainingNodes = nextNodes;
+
+        remainingComponents = nextComponents;
         ++x;
       }
-
-      //
-      // moveSinksRight(x); % KAK Don't move top predators from their assigned spot
-      scaleNodeBreadths((width - nodeWidth) / (x - 1));
     }
-  
+
+    function bfs(node, extractTargets) {
+      var queue = [node], currentCount = 1, nextCount = 0;
+      var x = 0;
+
+      while(currentCount > 0) {
+        var currentNode = queue.shift();
+        currentCount--;
+
+        if (!currentNode.hasOwnProperty('x')) {
+          currentNode.x = x;
+          currentNode.dx = nodeWidth;
+
+          var targets = extractTargets(currentNode);
+
+          queue = queue.concat(targets);
+          nextCount += targets.length;
+        }
+
+
+        if (currentCount == 0) { // level change
+          x++;
+          currentCount = nextCount;
+          nextCount = 0;
+        }
+
+      }
+    }
+  }
 
   function moveSourcesRight() {
     nodes.forEach(function(node) {
       if (!node.targetLinks.length) {
-        node.x = d3.min(node.sourceLinks, function(d) { return d.target.x; }) - 1;
+		  // node.x = d3.min(node.sourceLinks, function(d) { return d.target.x; }) - 1; // KAK Don't shift producers/detrital left
       }
     });
   }
@@ -263,7 +418,7 @@ d3.sankey = function() {
   function moveSinksRight(x) {
     nodes.forEach(function(node) {
       if (!node.sourceLinks.length) {
-        node.x = x - 1;
+        // node.x = x - 1;
       }
     });
   }
@@ -280,9 +435,10 @@ d3.sankey = function() {
         .sortKeys(d3.ascending)
         .entries(nodes)
         .map(function(d) { return d.values; });
-
+    
     initializeNodeDepth();
     resolveCollisions();
+
     for (var alpha = 1; iterations > 0; --iterations) {
       relaxRightToLeft(alpha *= .99);
       resolveCollisions();
@@ -301,7 +457,7 @@ d3.sankey = function() {
           node.dy = node.value * ky;
         });
       });
-
+		
       links.forEach(function(link) {
         link.dy = link.value * ky;
       });
@@ -408,68 +564,6 @@ d3.sankey = function() {
   function value(link) {
     return link.value;
   }
-
-  /* Cycle Related computations */
-  function markCycles() {
-    // ideally, find the 'feedback arc set' and remove them.
-    // This way is expensive, but should be fine for small numbers of links
-    var cycleMakers = [];
-    var addedLinks = new Array();
-    links.forEach(function(link) {
-      if( createsCycle( link.source, link.target, addedLinks ) ) {
-	    link.causesCycle=true;
-		link.cycleIndex = cycleMakers.length;
-        cycleMakers.push( link );
-      } else {
-        addedLinks.push(link);
-      }
-    });
-  };
-
-
-  function createsCycle( originalSource, nodeToCheck, graph ) {
-    if( graph.length == 0 ) {
-      return false;
-    }
-
-    var nextLinks = findLinksOutward( nodeToCheck, graph );
-    // leaf node check
-    if( nextLinks.length == 0 ) {
-      return false;
-    }
-
-    // cycle check
-    for( var i = 0; i < nextLinks.length; i++ ) {
-      var nextLink = nextLinks[i];
-
-      if( nextLink.target === originalSource ) {
-        return true;
-      }
-
-      // Recurse
-      if( createsCycle( originalSource, nextLink.target, graph ) ) {
-        return true;
-      }
-    }
-
-    // Exhausted all links
-    return false;
-  };
-
-  /* Given a node, find all links for which this is a source
-     in the current 'known' graph  */
-  function findLinksOutward( node, graph ) {
-    var children = [];
-
-    for( var i = 0; i < graph.length; i++ ) {
-      if( node == graph[i].source ) {
-        children.push( graph[i] );
-      }
-    }
-
-    return children;
-  }
-
 
   return sankey;
 };
